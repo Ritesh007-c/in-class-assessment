cae0680563fbb380ddc6026e7dc62b56463fb428
paul-laird <35932444+paul-laird@users.noreply.github.com>
2020-03-23 09:22:50 +0000

    Update portfolio.py

--- portfolio.py
+++ portfolio.py
@@ -1,9 +1,12 @@
 class Portfolio:
     def __init__(self,b={}):
-        self.__balance = b
+        self.__balance = b.copy()
         
     def getBalance(self,sym):
         if sym not in self.__balance:
             return 0
         return self.__balance[sym]
+    
+    def invest(self,sym,qty):
+        self.__balance[sym]= self.getBalance(sym)+qty
     
